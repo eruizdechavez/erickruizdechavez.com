86667eb2543f4d729bc8b6f07bf4571325f5166e
Erick Ruiz de Chavez <eruizdechavez@fastmail.com>
2016-11-24 11:48:33 -0500

    Arregla el build

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-jekyll build
-htmlproofer ./_site --check-html --disable-external
+bundle exec jekyll build
+bundle exec htmlproofer ./_site --check-html --disable-external
 # --url-ignore http://www.todopuebla.com/directorio/ondoibilitours/blog --file-ignore ./_site/sections/blogs.html
