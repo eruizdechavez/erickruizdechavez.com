0378a906b0bb6bd5c4e9e9e7392953869057e91f
Erick Ruiz de Chavez <erick@erch.co>
2021-01-07 06:21:06 +0000

    Fix images

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ const pluginRss = require("@11ty/eleventy-plugin-rss");
 const cacheBuster = require('@mightyplow/eleventy-plugin-cache-buster');
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("_content/images");
+
   eleventyConfig.addPlugin(pluginTailwindCSS, {
     src: "_includes/**/*.css",
     keepFolderStructure: false,
